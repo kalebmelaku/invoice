b551d435d76b9890ef4802f156d26a3533d37090
kaleb <kalebmelaku97@gmail.com>
2023-02-25 23:39:30 +0300

    invoice page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import './invoice.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="invoice-box">
+      <table cellPadding={0} cellSpacing={0}>
+        <tr className="top">
+          <td colSpan={2}>
+            <table>
+              <tr>
+                <td className="title">
+                  <h1>Company Name</h1>
+                  <button className="print_btn" onClick={() => { window.print(); }}>Print</button>
+                </td>
+              </tr>
+              <tr>
+
+							<td>
+								Invoice #: 123<br />
+								Created: January 1, 2015<br />
+								Due: February 1, 2015
+							</td>
+						</tr>
+            </table>
+
+          </td>
+        </tr>
+        <tr className="information">
+          <td colSpan={2}>
+          <table>
+						<tr>
+							<td>
+								Sparksuite, Inc.<br />
+								12345 Sunny Road<br />
+								Sunnyville, CA 12345
+							</td>
+
+							<td>
+								Acme Corp.<br />
+								John Doe<br />
+								john@example.com
+							</td>
+						</tr>
+					</table>
+          </td>
+        </tr>
+        <tr className="heading">
+				<td>Payment Method</td>
+
+				<td>Check #</td>
+			</tr>
+
+			<tr className="details">
+				<td>Check</td>
+
+				<td>1000</td>
+			</tr>
+
+			<tr className="heading">
+				<td>Item</td>
+
+				<td>Price</td>
+			</tr>
+
+			<tr className="item">
+				<td>Website design</td>
+
+				<td>$300.00</td>
+			</tr>
+
+			<tr className="item">
+				<td>Hosting (3 months)</td>
+
+				<td>$75.00</td>
+			</tr>
+
+			<tr className="item last">
+				<td>Domain name (1 year)</td>
+
+				<td>$10.00</td>
+			</tr>
+
+			<tr className="total">
+				<td></td>
+
+				<td>Total: $385.00</td>
+			</tr>
+      </table>
     </div>
   );
+  
 }
 
 export default App;
